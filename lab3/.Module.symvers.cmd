d6d3fb9304a4a9147368a92b7a026e97067d62ca
Eddie S <37740577+hotstreams@users.noreply.github.com>
2021-11-22 19:59:17 +0300

    Delete .Module.symvers.cmd

--- lab3/.Module.symvers.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/eddie/Dev/io/report-skeleton/lab3/Module.symvers := sed 's/ko$$/o/' /home/eddie/Dev/io/report-skeleton/lab3/modules.order | scripts/mod/modpost -m -a   -o /home/eddie/Dev/io/report-skeleton/lab3/Module.symvers -e -i Module.symvers   -T -
