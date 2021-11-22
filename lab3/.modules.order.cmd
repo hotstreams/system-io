4afa56ef64aaa26f61321e8f1a51977db881ee51
Eddie S <37740577+hotstreams@users.noreply.github.com>
2021-11-22 19:59:44 +0300

    Delete .modules.order.cmd

--- lab3/.modules.order.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/eddie/Dev/io/report-skeleton/lab3/modules.order := {   echo /home/eddie/Dev/io/report-skeleton/lab3/lab3.ko; :; } | awk '!x[$$0]++' - > /home/eddie/Dev/io/report-skeleton/lab3/modules.order
